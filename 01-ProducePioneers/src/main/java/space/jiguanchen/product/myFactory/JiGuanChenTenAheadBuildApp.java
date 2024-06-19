b1ba9834c1d58115456487953ea2a0f83a3223a0
jiguanchen <gchji@tsingsoft.com.cn>
2024-06-19 16:08:47 +0800

    fix: 优化抽象工厂主方法

--- 01-ProducePioneers/src/main/java/space/jiguanchen/product/myFactory/JiGuanChenTenAheadBuildApp.java
+++ 01-ProducePioneers/src/main/java/space/jiguanchen/product/myFactory/JiGuanChenTenAheadBuildApp.java
@@ -3,7 +3,7 @@ package space.jiguanchen.product.myFactory;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * @Description: 10-AheadModelApp  未来十年模型
+ * @Description: JiGuanChenTenAheadBuildApp 主类，展示未来十年模型应用
  * @Author: jiguanchen
  * @Date: 2024/5/30 21:09
  */
@@ -11,16 +11,22 @@ import lombok.extern.slf4j.Slf4j;
 public class JiGuanChenTenAheadBuildApp {
 
     public static void main(String[] args) {
-        ContinueWorkingFactory continueWorkingFactory = new ContinueWorkingFactory();
-        TenAheadModel tenAheadModel = continueWorkingFactory.createTenAheadModel();
-        tenAheadModel.tenAheadToLive();
+        // 使用 ContinueWorkingFactory 创建模型并展示
+        runFactory(new ContinueWorkingFactory());
 
-        StartupCompanyFactory startupCompanyFactory = new StartupCompanyFactory();
-        tenAheadModel = startupCompanyFactory.createTenAheadModel();
-        tenAheadModel.tenAheadToLive();
+        // 使用 StartupCompanyFactory 创建模型并展示
+        runFactory(new StartupCompanyFactory());
 
-        ToQingDaoLivingFactory toQingDaoLivingFactory = new ToQingDaoLivingFactory();
-        tenAheadModel = toQingDaoLivingFactory.createTenAheadModel();
-        tenAheadModel.tenAheadToLive();
+        // 使用 ToQingDaoLivingFactory 创建模型并展示
+        runFactory(new ToQingDaoLivingFactory());
+    }
+
+    /**
+     * @Description: 工厂方法运行模型
+     * @param factory TenAheadModelFactory 工厂实例
+     */
+    private static void runFactory(TenAheadModelFactory factory) {
+        TenAheadModel model = factory.createTenAheadModel();
+        model.tenAheadToLive();
     }
 }
